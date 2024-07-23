c3994b2ffa5f0f2e9399aaa26064a4de6d7ff9df
Aditya Kumar Singh <165510438+adityaFunex@users.noreply.github.com>
2024-07-23 19:57:22 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -181,3 +181,37 @@ $(document).ready(function () {
 
 
 
+$(document).ready(function () {
+    $('.clientTestimonial-box').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: true,
+        autoplaySpeed: 1500,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    var dotButtons = document.querySelectorAll('button.slick-arrow');
+    dotButtons.forEach(function (button) {
+        button.innerHTML = '';
+    });
+});
+
+
